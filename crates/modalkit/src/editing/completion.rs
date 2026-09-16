36c24eef4a9314f926f46bf5fc53a7c4d7c9445b
Ulyssa <git@ulyssa.dev>
2026-09-15 23:55:58 -0400

    fix tests on windows

--- crates/modalkit/src/editing/completion.rs
+++ crates/modalkit/src/editing/completion.rs
@@ -394,6 +394,9 @@ mod parse {
         is_not("\t\n\\ |\"")(input)
     }
 
+    /// Parse a path and unescape special characters at the start of the string.
+    ///
+    /// Note that this expects `\` to be escaped, even on Windows.
     fn parse_filepath(input: &str) -> IResult<&str, String> {
         escaped_transform(
             filepath_char,
@@ -438,6 +441,9 @@ mod parse {
         filepath_at_end(prefix.as_ref()).ok().map(|(_, path)| path)
     }
 
+    /// Escape special characters in the in input string.
+    ///
+    /// Note that this escapes `\`, even on Windows.
     pub fn escape_string(input: &mut String) {
         for c in ["\\\\", "\\ ", "\\#", "\\%", "\\|", "\\\""] {
             if input.contains(&c[1..2]) {
@@ -491,10 +497,10 @@ pub fn complete_path(input: &EditRope, cursor: &mut Cursor) -> Vec<String> {
         // Expand plain tilde to the home directory:
         cursor.left(parse::escaped_len(filepath_in.as_ref()));
 
-        let mut home = filepath_expanded.into_owned();
+        let mut home = format!("{}{MAIN_SEPARATOR}", filepath_expanded.into_owned());
         parse::escape_string(&mut home);
 
-        return vec![format!("{home}{MAIN_SEPARATOR}")];
+        return vec![home];
     }
 
     if filepath_expanded.is_empty() {
@@ -637,6 +643,7 @@ fn sort_paths(paths: &mut [String]) {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::path::MAIN_SEPARATOR_STR;
 
     fn mkmap() -> CompletionMap<String, usize> {
         let mut map = CompletionMap::default();
@@ -751,6 +758,8 @@ mod tests {
 
     #[test]
     fn test_complete_path_expand_env_simple() {
+        std::env::set_var("HOME", "/home/user");
+
         let rope = EditRope::from("sp $HOME");
         let mut cursor = Cursor::new(0, 8);
         let res = complete_path(&rope, &mut cursor);
@@ -772,7 +781,10 @@ mod tests {
         let rope = EditRope::from("sp ~");
         let mut cursor = Cursor::new(0, 4);
         let res = complete_path(&rope, &mut cursor);
-        assert_eq!(res, vec![std::env::var("HOME").unwrap() + "/"]);
+        let mut exp =
+            std::env::home_dir().unwrap().to_string_lossy().into_owned() + MAIN_SEPARATOR_STR;
+        parse::escape_string(&mut exp);
+        assert_eq!(res, vec![exp]);
         assert_eq!(cursor.get_x(), 3);
     }
 
@@ -791,6 +803,8 @@ mod tests {
 
     #[test]
     fn test_complete_path_expand_cursor_multibyte() {
+        std::env::set_var("HOME", "/home/user");
+
         // The cursor moves over characters (and not bytes or columns):
         let rope = EditRope::from("sp $HOME/文");
         let mut cursor = Cursor::new(0, 10);
@@ -801,6 +815,8 @@ mod tests {
 
     #[test]
     fn test_complete_path_expand_cursor_escaped() {
+        std::env::set_var("HOME", "/home/user");
+
         // Cursor needs to move back over the original escaped text, and not the unescaped text:
         let rope = EditRope::from("sp $HOME/a\\ b");
         let mut cursor = Cursor::new(0, 13);
@@ -811,6 +827,8 @@ mod tests {
 
     #[test]
     fn test_complete_path_cursor_multibyte() {
+        std::env::set_var("HOME", "/home/user");
+
         let rope = EditRope::from(format!("sp .{MAIN_SEPARATOR}文"));
         let mut cursor = Cursor::new(0, 6);
         let res = complete_path(&rope, &mut cursor);
